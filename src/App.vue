2c5fcaf283610cea387e69efbccaec1f39745cf7
onetuks <onetuks@naver.com>
2024-05-22 15:24:23 +0900

    Feat: 찜 아이콘 컴포넌트 구현

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ZzimIcon />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import ZzimIcon from './components/zzim/ZzimIcon.vue';
 
-export default {
-  name: 'App',
+@Options({
   components: {
-    HelloWorld
-  }
-}
+    ZzimIcon,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
